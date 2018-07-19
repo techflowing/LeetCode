895b8f8dcdd50677bc521908666844d067de8bd7
techflowing <techflowing@gmail.com>
2018-07-19 22:19:43 +0800

    【创项】

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * LeetCode 刷题项目
+ * 1. 文件命名方式：题目难度_编号_题目名称_方法
+ * 1.1 题目难度：E：简单    M：中等    H：困难
+ * <p>
+ * 2. 注释说明
+ * 2.1 类注释描述题目
+ * 2.2 核心方法注释描述结题思路
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
